de31150de53fbfdc828ad70db40a15406494e609
Joan01215 <joangalaxy07@gmail.com>
2025-11-27 17:10:04 -0600

    Add files via upload

---
+++ 25550488_EXA2/src/pkg25550488_exa2/Main.java
@@ -0,0 +1,37 @@
+package pkg25550488_exa2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1, num2, opcion;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Bienvenido");
+        System.out.println("Elija dos numeros del 2 en delante");
+        System.out.println("Capture el primer numero");
+        num1 = captu.nextInt();
+        System.out.println("Capture el segundo numero");
+        num2 = captu.nextInt();
+        System.out.println("Quiere imprimir solo los primos? (1 para si, 2 para no)");
+        opcion = captu.nextInt();
+        if(opcion == 1){
+        for( int i = num1; i <=num2; i++){
+            for(int j= 1; j<= i; j++)
+                System.out.print("*");
+        
+        System.out.println("");    
+        }
+        }else if(opcion == 2){
+        for( int i = num1; i <=num2; i++){
+            for(int j= 1; j<= i; j++)
+                System.out.print("*");
+        
+        System.out.println("");
+        }
+        
+        
+    }
+    }
+}
+    
